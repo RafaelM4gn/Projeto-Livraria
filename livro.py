271a375584a880121ff413f8bb57629258b419a5
JhosefMatheus <jhosef.mat123@hotmail.com>
2021-02-02 12:19:25 -0300

    Alterações feitas: -Exclusão do livros.db -Atualização DBHelper.py para que o id fosse incrementado automaticamente e que ele não precisasse receber o id na hora do cadastro do livro -Atualização na classe livro.py para que ele não recebesse como um dos parâmetros para seu construtor o id, tendo em vista que o mesmo não será mais necessário -Criação da Interface.py

--- livro.py
+++ livro.py
@@ -1,6 +1,5 @@
 class Livro:
-    def __init__(self, id, nome, autor, editora, proprietario):
-        self.id = id
+    def __init__(self, nome, autor, editora, proprietario):
         self.nome = nome
         self.autor = autor
         self.editora = editora
